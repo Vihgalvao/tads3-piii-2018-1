819c5218e823f5dbd0ea992fc3be752e94e7fae3
Victor Galvão <victorgabrielsg@gmail.com>
2018-02-27 20:52:45 -0300

    Primeiro Envio.

---
+++ agenda/src/main/java/br/senac/tads/pi3b/agenda/Pessoa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3b.agenda;
+
+/**
+ *
+ * @author victor.gsgalvao
+ */
+public class Pessoa {
+    
+    long id;
+    
+    String nome;
+    
+    Date dtnascimento;
+    
+    
+    
+    
+}
